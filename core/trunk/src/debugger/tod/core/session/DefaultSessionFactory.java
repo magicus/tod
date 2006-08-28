e8c89c1db49cda05701a24db47563b6222f4bae5
gpothier <gpothier@52faa79f-a2d0-0310-bd40-81a52920883b>
2006-08-28 23:30:26 +0000

    Disabled DefaultSessionFactory

--- core/trunk/src/debugger/tod/core/session/DefaultSessionFactory.java
+++ core/trunk/src/debugger/tod/core/session/DefaultSessionFactory.java
@@ -5,33 +5,31 @@ package tod.core.session;
 
 import java.net.URI;
 
-import tod.impl.bci.asm.ASMLocalSession;
-
-public class DefaultSessionFactory implements ISessionFactory
-{
-	private static DefaultSessionFactory INSTANCE = new DefaultSessionFactory();
-
-	public static DefaultSessionFactory getInstance()
-	{
-		return INSTANCE;
-	}
-
-	private DefaultSessionFactory()
-	{
-	}
-
-	public ISession createSession(
-			URI aLocation, 
-			String aGlobalWorkingSet, 
-			String aIdentificationWorkingSet,
-			String aTraceWorkingSet)
-	{
-		return new ASMLocalSession(aLocation, aGlobalWorkingSet, aIdentificationWorkingSet, aTraceWorkingSet);
-	}
-
-	public ISession loadSession(URI aLocation)
-	{
-		throw new UnsupportedOperationException();
-	}
-
-}
+//public class DefaultSessionFactory implements ISessionFactory
+//{
+//	private static DefaultSessionFactory INSTANCE = new DefaultSessionFactory();
+//
+//	public static DefaultSessionFactory getInstance()
+//	{
+//		return INSTANCE;
+//	}
+//
+//	private DefaultSessionFactory()
+//	{
+//	}
+//
+//	public ISession createSession(
+//			URI aLocation, 
+//			String aGlobalWorkingSet, 
+//			String aIdentificationWorkingSet,
+//			String aTraceWorkingSet)
+//	{
+//		return new ASMLocalSession(aLocation, aGlobalWorkingSet, aIdentificationWorkingSet, aTraceWorkingSet);
+//	}
+//
+//	public ISession loadSession(URI aLocation)
+//	{
+//		throw new UnsupportedOperationException();
+//	}
+//
+//}
