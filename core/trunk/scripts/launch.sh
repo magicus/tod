58cb90245124c29d5262d3c8ca18269cafb0724c
gpothier <gpothier@52faa79f-a2d0-0310-bd40-81a52920883b>
2006-09-10 03:18:28 +0000

    More script fixes

--- core/trunk/scripts/launch.sh
+++ core/trunk/scripts/launch.sh
@@ -1,11 +1,12 @@
 #! /bin/sh
 
-$JAVA_HOME/bin/java -Xmx512m \
--Djava.library.path=$NATIVE \
--ea \
--server \
--cp $CLASSPATH \
--Dnode-data-dir=$DATA_DIR \
--Dmaster-host=$MASTER_HOST \
--Devents-file=$EVENTS_FILE \
-$1 $2 $3 $4 $5
+$JAVA_HOME/bin/java -Xmx512m\
+ -Djava.library.path=$NATIVE\
+ -ea\
+ -server\
+ -cp $CLASSPATH\
+ -Dnode-data-dir=$DATA_DIR\
+ -Dmaster-host=$MASTER_HOST\
+ -Devents-file=$EVENTS_FILE\
+ -Dlocations-file=$LOCATIONS_FILE\
+ $1 $2 $3 $4 $5
