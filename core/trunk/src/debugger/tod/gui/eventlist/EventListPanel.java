50ec993f9870da5b99b58e5e7e9ea636742f6220
gpothier <gpothier@52faa79f-a2d0-0310-bd40-81a52920883b>
2007-12-19 21:41:54 +0000

    Complete - task 3: Provide a way to go to a specific event in EventList Done

--- core/trunk/src/debugger/tod/gui/eventlist/EventListPanel.java
+++ core/trunk/src/debugger/tod/gui/eventlist/EventListPanel.java
@@ -21,7 +21,10 @@ RSA Data Security, Inc. MD5 Message-Digest Algorithm".
 package tod.gui.eventlist;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
 import java.util.HashMap;
@@ -29,6 +32,7 @@ import java.util.List;
 import java.util.Map;
 
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
 import tod.core.database.browser.IEventBrowser;
@@ -309,10 +313,29 @@ implements MouseWheelListener
 		};
 		itsEventsPanel.setOpaque(false);
 		itsEventsPanel.addMouseWheelListener(this);
+		
+		final JTextField theGotoEventField = new JTextField(20);
+		theGotoEventField.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent aE)
+			{
+				String theText = theGotoEventField.getText();
+				try
+				{
+					long theTimestamp = Long.parseLong(theText);
+					setTimestamp(theTimestamp);
+				}
+				catch (NumberFormatException e)
+				{
+					theGotoEventField.setText("invalid");
+				}
+			}
+		});
 
 		
 		setLayout(new BorderLayout());
 		add(itsEventsPanel, BorderLayout.CENTER);
+		add(theGotoEventField, BorderLayout.NORTH);
 		add(itsScroller, BorderLayout.EAST);
 	}
 	
