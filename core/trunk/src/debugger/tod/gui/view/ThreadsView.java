8543a6a153a3b62ba9fd44d5468504ecb58dc647
omotelet <omotelet@52faa79f-a2d0-0310-bd40-81a52920883b>
2008-02-08 21:54:20 +0000

    NEW - # 37: Add zoom feature on murals http://pleiad.dcc.uchile.cl/trac/tod/ticket/37
    Added a range slider in thread view.
    Moving range bounds modify the range of the events displayed by the murals
    WARNING moving lower bound of the range does not work

--- core/trunk/src/debugger/tod/gui/view/ThreadsView.java
+++ core/trunk/src/debugger/tod/gui/view/ThreadsView.java
@@ -20,17 +20,26 @@ RSA Data Security, Inc. MD5 Message-Digest Algorithm".
 */
 package tod.gui.view;
 
+import infovis.panel.dqinter.DoubleRangeSlider;
+
 import java.awt.BorderLayout;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.Timer;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import tod.core.database.browser.ILogBrowser;
 import tod.core.database.structure.IThreadInfo;
@@ -55,6 +64,10 @@ public class ThreadsView extends LogView
 	private JLabel itsEventsCountLabel;
 	private long itsLastEventCount = -1;
 
+	//slider with double handles defining the timestamp range in the murals
+	private TimestampRangeSlider itsTimestampSlider;
+	
+	
 	private Timer itsTimer;
 	
 	public ThreadsView(IGUIManager aGUIManager, ILogBrowser aLog, ThreadsSeed aSeed)
@@ -69,10 +82,16 @@ public class ThreadsView extends LogView
 	{
 		itsDock = new SequenceViewsDock(getGUIManager());
 		itsEventsCountLabel = new JLabel();
+		itsTimestampSlider = new TimestampRangeSlider();
+		Box theTopBox = Box.createHorizontalBox();
+		theTopBox.add(itsEventsCountLabel);
+		theTopBox.add(Box.createHorizontalGlue());
+		theTopBox.add(itsTimestampSlider);
+		theTopBox.add(Box.createHorizontalGlue());
 		
 		setLayout(new BorderLayout());
+		add (theTopBox, BorderLayout.NORTH);
 		add (itsDock, BorderLayout.CENTER);
-		add (itsEventsCountLabel, BorderLayout.NORTH);
 		
 		update();
 		
@@ -118,9 +137,108 @@ public class ThreadsView extends LogView
 					itsDock.pSeeds().add(theSeed);
 				}
 			}
-			
-			itsDock.pStart().set(getLogBrowser().getFirstTimestamp());
-			itsDock.pEnd().set(getLogBrowser().getLastTimestamp());
+			//TODO check why not working
+			//itsDock.pStart().set(itsTimestampSlider.itsStartRangeTimestamp);
+			itsDock.pStart().set(itsTimestampSlider.itsStartTimestamp);
+			itsDock.pEnd().set(itsTimestampSlider.itsEndRangeTimestamp);
+			itsTimestampSlider.updateLastLabel();
 		}
 	}
+	
+	/**
+	 * shows a range slider for timestamp 
+	 * it displays the fist and last timestamps above the slider 
+	 * and the first and last timestamps of the chosen range below the slider 
+	 * @author omotelet
+	 */
+	private class TimestampRangeSlider extends JPanel implements ChangeListener{
+
+		private DoubleRangeSlider itsSlider;
+		private JLabel itsStartLabel = new JLabel(), itsEndLabel= new JLabel(), 
+						itsStartRangeLabel= new JLabel(), itsEndRangeLabel= new JLabel();
+		private long itsStartTimestamp, itsEndTimestamp, itsStartRangeTimestamp, itsEndRangeTimestamp;
+		
+		public TimestampRangeSlider()
+		{
+			itsSlider = new DoubleRangeSlider(0, 1, 0, 1){
+				@Override
+				public String getToolTipText()
+				{
+					return "modify the range in the slider in order to zoom in or out in the event murals";
+				} 
+			};
+			
+			itsSlider.getModel().addChangeListener(this);
+	
+			itsStartLabel.setText(0+"ms");
+			updateLastLabel();
+			updateRangeLabels();
+
+			initLayout();
+		}
+		
+		private void updateRangeLabels()
+		{
+			double theLow = itsSlider.getLowValue()*(itsEndTimestamp-itsStartTimestamp);
+			itsStartRangeTimestamp=(long) (itsStartTimestamp + theLow);
+			double theHigh = itsSlider.getHighValue()*(itsEndTimestamp-itsStartTimestamp);
+			itsEndRangeTimestamp=(long) (itsStartTimestamp + theHigh);
+			
+			itsStartRangeLabel.setText((int)(theLow/1000000)+"ms <");
+			itsEndRangeLabel.setText("< "+(int)(theHigh/1000000)+"ms");
+		}
+		
+		private void updateLastLabel(){
+			boolean rangeNeedUpdate = itsEndTimestamp==itsEndRangeTimestamp;
+			
+			itsStartTimestamp = getLogBrowser().getFirstTimestamp();
+			itsEndTimestamp = getLogBrowser().getLastTimestamp();
+			itsEndLabel.setText(""+(itsEndTimestamp-itsStartTimestamp)/1000000+"ms");
+			if (rangeNeedUpdate) updateViews();
+		}
+		
+		private void initLayout()
+		{
+			setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
+			itsSlider.setEnabled(true);	
+			
+			Font theDerivedFont = itsStartLabel.getFont().deriveFont(9f);
+			itsStartLabel.setFont(theDerivedFont);
+			itsEndLabel.setFont(theDerivedFont);
+			itsStartRangeLabel.setFont(theDerivedFont);
+			itsEndRangeLabel.setFont(theDerivedFont);
+			
+			Box theTopBox = Box.createHorizontalBox();
+			theTopBox.add(itsStartLabel);
+			theTopBox.add(Box.createHorizontalGlue());
+			theTopBox.add(itsStartRangeLabel);
+			theTopBox.add(Box.createHorizontalGlue());
+			theTopBox.add(itsEndRangeLabel);
+			theTopBox.add(Box.createHorizontalGlue());
+			theTopBox.add(itsEndLabel);
+			
+			add(theTopBox);
+			add(itsSlider);
+		
+		}
+		
+		public void stateChanged(ChangeEvent aE)
+		{
+			updateViews();
+		}
+
+		private void updateViews()
+		{
+			updateRangeLabels();
+			//update views
+			if (!itsSlider.getModel().getValueIsAdjusting()){
+				//TODO check why not working
+				//itsDock.pStart().set(itsStartRangeTimestamp);
+				itsDock.pEnd().set(itsEndRangeTimestamp);
+			}
+		}
+		
+		
+	}
+	
 }
