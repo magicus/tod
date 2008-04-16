4213a99aeb245aa53a4588985f37c0e5b67607b7
omotelet <omotelet@52faa79f-a2d0-0310-bd40-81a52920883b>
2008-04-16 10:05:05 +0000

    NEW - # 18: Problem of string search https://sylapse.com:3255/trac/tod/ticket/18 attempt to fix the indexer optimization. NOT TESTED --

--- dbgrid/trunk/src/main/tod/impl/dbgrid/db/StringIndexer.java
+++ dbgrid/trunk/src/main/tod/impl/dbgrid/db/StringIndexer.java
@@ -17,7 +17,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 Parts of this work rely on the MD5 algorithm "derived from the 
 RSA Data Security, Inc. MD5 Message-Digest Algorithm".
-*/
+ */
 package tod.impl.dbgrid.db;
 
 import java.io.File;
@@ -43,34 +43,42 @@ import org.apache.lucene.search.Searcher;
 import tod.core.config.TODConfig;
 import tod.impl.database.IBidiIterator;
 import tod.impl.dbgrid.dispatch.RINodeConnector.StringSearchHit;
+import tod.utils.TODUtils;
 import zz.utils.Utils;
 
 public class StringIndexer
 {
 	private static final String FIELD_ID = "id";
+
 	private static final String FIELD_VALUE = "value";
 
 	private final TODConfig itsConfig;
+
 	private final File itsFile;
-	
+
 	/**
 	 * Lucene index writer for string indexing
 	 */
 	private IndexWriter itsIndexWriter;
-	
+
 	private Analyzer itsAnalyzer;
+
+	private int itsIndexedDocumentNumber;
+
+	private int itsOptimizedIndexedDocumentNumber;
 
 	public StringIndexer(TODConfig aConfig, File aFile)
 	{
 		itsConfig = aConfig;
 		itsFile = aFile;
-		
-		itsIndexWriter = null; // In case creation fails - we don't want a stale index
+
+		itsIndexWriter = null; // In case creation fails - we don't want a
+		// stale index
 		try
 		{
 			Utils.rmDir(itsFile);
 			itsAnalyzer = new NeuneuAnalyzer();
-//			itsAnalyzer = new SimpleAnalyzer();
+			// itsAnalyzer = new SimpleAnalyzer();
 			itsIndexWriter = new IndexWriter(itsFile, itsAnalyzer, true);
 		}
 		catch (IOException e)
@@ -79,40 +87,42 @@ public class StringIndexer
 		}
 
 	}
-	
+
 	public void register(long aId, String aString)
 	{
 		Document theDocument = new Document();
-		theDocument.add(new Field(
-				FIELD_ID, 
-				Long.toString(aId), 
-				Field.Store.YES, 
-				Field.Index.NO));
-		
-		theDocument.add(new Field(
-				FIELD_VALUE,
-				aString,
-				Field.Store.NO, 
-				Field.Index.TOKENIZED));
-		
+		theDocument.add(new Field(FIELD_ID, Long.toString(aId), Field.Store.YES, Field.Index.NO));
+
+		theDocument.add(new Field(FIELD_VALUE, aString, Field.Store.NO, Field.Index.TOKENIZED));
+
 		try
 		{
 			itsIndexWriter.addDocument(theDocument);
+			TODUtils.logf(1, "Adding string: %1 to string indexer ", aString);
+			itsIndexedDocumentNumber++;
 		}
 		catch (IOException e)
 		{
 			throw new RuntimeException(e);
 		}
-		
+
 	}
 
 	public HitIterator search(String aText)
 	{
 		try
 		{
-			itsIndexWriter.optimize();
-			itsIndexWriter.close();
-			
+			if (itsIndexedDocumentNumber > 0)
+			// optimize only when a substantial change has been done (30%)
+			if (itsOptimizedIndexedDocumentNumber * 100 / itsIndexedDocumentNumber < 70)
+			{
+				itsIndexWriter.optimize();
+				itsOptimizedIndexedDocumentNumber = itsIndexedDocumentNumber;
+			}
+
+			// TODO check the need for closing the indexwriter
+			// itsIndexWriter.close();
+
 			IndexReader theReader = IndexReader.open(itsFile);
 			Searcher theSearcher = new IndexSearcher(theReader);
 			QueryParser theParser = new QueryParser(FIELD_VALUE, itsAnalyzer);
@@ -125,17 +135,18 @@ public class StringIndexer
 			throw new RuntimeException(e);
 		}
 	}
-	
+
 	private static class HitIterator implements IBidiIterator<StringSearchHit>
 	{
 		private Hits itsHits;
+
 		private int itsIndex;
 
-		public HitIterator(Hits aHits) 
+		public HitIterator(Hits aHits)
 		{
 			itsHits = aHits;
 		}
-		
+
 		private long getScore(float aLuceneScore)
 		{
 			return (long) (1000 - (aLuceneScore * 1000));
@@ -145,18 +156,17 @@ public class StringIndexer
 		{
 			return itsIndex < itsHits.length();
 		}
-		
-		public StringSearchHit next() 
+
+		public StringSearchHit next()
 		{
-			if (! hasNext()) throw new NoSuchElementException();
-			
+			if (!hasNext()) throw new NoSuchElementException();
+
 			try
 			{
 				Document theDoc = itsHits.doc(itsIndex);
-				StringSearchHit theHit = new StringSearchHit(
-						Long.parseLong(theDoc.get("id")),
-						getScore(itsHits.score(itsIndex)));
-				
+				StringSearchHit theHit = new StringSearchHit(Long.parseLong(theDoc.get("id")), getScore(itsHits
+						.score(itsIndex)));
+
 				itsIndex++;
 				return theHit;
 			}
@@ -170,18 +180,17 @@ public class StringIndexer
 		{
 			return itsIndex > 0;
 		}
-		
+
 		public StringSearchHit previous()
 		{
-			if (! hasPrevious()) throw new NoSuchElementException();
+			if (!hasPrevious()) throw new NoSuchElementException();
 			try
 			{
 				--itsIndex;
 				Document theDoc = itsHits.doc(itsIndex);
-				StringSearchHit theHit = new StringSearchHit(
-						Long.parseLong(theDoc.get(FIELD_ID)),
-						getScore(itsHits.score(itsIndex)));
-				
+				StringSearchHit theHit = new StringSearchHit(Long.parseLong(theDoc.get(FIELD_ID)), getScore(itsHits
+						.score(itsIndex)));
+
 				return theHit;
 			}
 			catch (IOException e)
@@ -207,19 +216,19 @@ public class StringIndexer
 		public TokenStream tokenStream(String aFieldName, Reader aReader)
 		{
 			return new NeuneuTokenizer(aReader);
-//			return new DelegateTokenizer(new LowerCaseTokenizer(aReader));
+			// return new DelegateTokenizer(new LowerCaseTokenizer(aReader));
 		}
 	}
-	
+
 	private static class DelegateTokenizer extends TokenStream
 	{
 		private Tokenizer itsTokenizer;
-		
+
 		public DelegateTokenizer(Tokenizer aTokenizer)
 		{
 			itsTokenizer = aTokenizer;
 		}
-		
+
 		@Override
 		public Token next() throws IOException
 		{
@@ -227,21 +236,24 @@ public class StringIndexer
 			System.out.println(theToken);
 			return theToken;
 		}
-		
+
 	}
-	
+
 	/**
-	 * This tokenizer splits strings at each change between letter and non letter.
-	 * Inspired of {@link CharTokenizer}
+	 * This tokenizer splits strings at each change between letter and non
+	 * letter. Inspired of {@link CharTokenizer}
+	 * 
 	 * @author gpothier
 	 */
 	private static class NeuneuTokenizer extends Tokenizer
 	{
 		private int itsOffset = 0;
+
 		private StringBuilder itsBuilder = new StringBuilder();
+
 		private boolean itsWasLetter = false;
-		
-		public NeuneuTokenizer(Reader input) 
+
+		public NeuneuTokenizer(Reader input)
 		{
 			super(input);
 		}
@@ -251,55 +263,55 @@ public class StringIndexer
 		 * Returns true if the given char starts a new token
 		 */
 		protected boolean isNewToken(char c)
-		{ 
+		{
 			boolean theIsLetter = Character.isLetter(c);
 			boolean theResult = theIsLetter != itsWasLetter;
 			itsWasLetter = theIsLetter;
 			return theResult;
 		}
-		  
+
 		/**
-		 * Returns true if the given char is valid as part of a token. If
-		 * not, it will not be included in the token and a new token will be
-		 * started
+		 * Returns true if the given char is valid as part of a token. If not,
+		 * it will not be included in the token and a new token will be started
 		 */
 		protected boolean isValid(char c)
 		{
-			return ! Character.isWhitespace(c);  
+			return !Character.isWhitespace(c);
 		}
-		  
+
 		/**
-		 * Called on each token character to normalize it before it is added
-		 * to the token. The default implementation does nothing. Subclasses
-		 * may use this to, e.g., lowercase tokens.
+		 * Called on each token character to normalize it before it is added to
+		 * the token. The default implementation does nothing. Subclasses may
+		 * use this to, e.g., lowercase tokens.
 		 */
-		protected char normalize(char c) 
+		protected char normalize(char c)
 		{
 			return Character.toLowerCase(c);
 		}
 
-		/** 
-		 * Returns the next token in the stream, or null at EOS. 
+		/**
+		 * Returns the next token in the stream, or null at EOS.
 		 */
+		@Override
 		public final Token next() throws IOException
 		{
 			String theToken = null;
-			
+
 			while (true)
 			{
 				int c = input.read();
 				itsOffset++;
-				if (c == -1) 
+				if (c == -1)
 				{
 					if (itsBuilder != null && itsBuilder.length() > 0) theToken = itsBuilder.toString();
 					itsBuilder = null;
 					break;
 				}
-				
+
 				char ch = (char) c;
-				if (! isValid(ch))
+				if (!isValid(ch))
 				{
-					if (itsBuilder.length() > 0) 
+					if (itsBuilder.length() > 0)
 					{
 						theToken = itsBuilder.toString();
 						itsBuilder.delete(0, itsBuilder.length());
@@ -322,7 +334,7 @@ public class StringIndexer
 					itsBuilder.append(normalize(ch));
 				}
 			}
-			
+
 			if (theToken != null)
 			{
 				return new Token(theToken, itsOffset, itsOffset + theToken.length());
