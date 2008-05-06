af48be417b8943d6d4232062e854b22501483d43
minostro <minostro@52faa79f-a2d0-0310-bd40-81a52920883b>
2008-05-06 13:42:44 +0000

    se sacan objects, dataTypes y events

--- pytod/trunk/python-project/src/debugger/pytod/core/hunterTrace.py
+++ pytod/trunk/python-project/src/debugger/pytod/core/hunterTrace.py
@@ -16,34 +16,7 @@ import socket
 th = False
 if th:
     from threading import settrace
-
-objects = {
-           'class':0,
-           'method':1,
-           'attribute':2,
-           'function':3,
-           'local':4,
-           'probe':5,
-           'thread':6
-           }
-
-dataTypes = {
-             int.__name__:0,
-             str.__name__:1,
-             float.__name__:2,
-             long.__name__:3,
-             bool.__name__:4,
-             tuple.__name__:5,
-             list.__name__:6,
-             dict.__name__:7
-             }
-
-events = {
-          'register':0,
-          'call':1,
-          'set':2,
-          'return':3
-          }
+from constantsObjects import *
 
 class Diccionario(dict):
 
@@ -662,7 +635,7 @@ class hunterTrace(object):
         print classBases
         self.packer.pack_int(0)
         raw_input()
-        self.socket.sendall("milton")
+        self.socket.sendall(self.packer.get_buffer())
         objClass = self.__addClass__(classId,self.__createlnotab__(code),code)
         self.Id.__next__()
         #se deben registrar los metodos asociados 
