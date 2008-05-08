745f02600485228475088a6cbdcb6a8167624fb9
minostro <minostro@52faa79f-a2d0-0310-bd40-81a52920883b>
2008-05-08 23:57:43 +0000

    bug extraño con el uso de xdrlib, se deshabilita depurador cuando se envían los datos.

--- pytod/trunk/python-project/src/debugger/pytod/core/hunterTrace.py
+++ pytod/trunk/python-project/src/debugger/pytod/core/hunterTrace.py
@@ -15,7 +15,6 @@ import socket
 import inspect
 from constantsObjects import events, objects, dataTypes, packXDRLib
 from generatorId import generatorId
-from Descriptor import Descriptor
 from objectClass import Class
 from objectMethod import Method
 from objectFunction import Function
@@ -176,7 +175,7 @@ class hunterTrace(object):
             currentDepth = frameBack.f_locals['__depthFrame__']
             frame.f_locals['__depthFrame__'] = currentDepth + 1
         else:
-            frame.f_locals['__depthFrame__'] = 0
+            frame.f_locals['__depthFrame__'] = 1
         return frame.f_locals['__depthFrame__']
     
     def __timeStampFrame__(self, frame):
@@ -335,7 +334,7 @@ class hunterTrace(object):
         print self.events['return'],
         self.packer.pack_int(self.events['return'])
         #TODO: ver tipos de datos y la manera de enviarlos
-        print arg,
+        #print arg,
         self.packer.pack_int(1)
         print probeId,
         self.packer.pack_int(probeId)
@@ -605,6 +604,88 @@ class hunterTrace(object):
         print '======='
 
 hT = hunterTrace(generatorId(),generatorId(),generatorId(),xdrlib.Packer())
+
+class Descriptor(object):
+
+    def __setattr__(self, name, value):
+        import sys
+        frame = sys._getframe()
+        currentLasti = frame.f_lasti
+        currentDepth = hT.__getDepthFrame__(frame)
+        currentTimeStamp = time.time() 
+        parentTimeStamp = hT.__getTimeStampParentFrame__(frame, currentTimeStamp)
+        threadId = hT.__getThreadId__(thread.get_ident())
+        id = hT.Id.__get__()
+        key = type(self).__name__
+        key = hT.__getClassKey__(key)
+        if key == None:
+            return
+        obj = hT._class[key] 
+        objId = obj.__getId__()
+        print 'voy bien'
+        raw_input()
+        #comportamiento extraño
+        #se debe deshabilitar settrace
+        #revizar comportamiento de xdrlib
+        import sys
+        sys.settrace(None)
+        obj.attributes.__updateAttr__({name:id},objId)
+        hT.Id.__next__()
+        #registramos un nuevo probe        
+        if not hT._probe.has_key((currentLasti,objId)):
+            probeId = hT.__registerProbe__(currentLasti,objId)
+        else:
+            probeId = hT._probe[(currentLasti,objId)]          
+        hT.packer.reset()
+        print hT.events['set'],
+        hT.packer.pack_int(hT.events['set'])
+        print hT.objects['attribute'],
+        hT.packer.pack_int(hT.objects['attribute'])
+        #print 'id =',id,
+        print id,
+        hT.packer.pack_int(id)
+        #print 'target =',objId,
+        print objId,
+        hT.packer.pack_int(objId)
+        #print 'value =',value,
+        #TODO: ver caso cuando value es del tipo tuple, list, dict
+        #TODO: ver el asunto de los tipos de datos
+        #tomar el tipo y consultar en dataTypes el id
+        dataType = 8
+        if hT.dataTypes.has_key(value.__class__.__name__):
+            dataType = hT.dataTypes[value.__class__.__name__]
+        hT.packer.pack_int(dataType)
+        print dataType,
+        #se crea una estructura la cual retorna
+        #que metodo debe ser llamado
+        if hT.packXDRLib.has_key(dataType):
+            methodName = hT.packXDRLib[dataType]
+            getattr(hT.packer,'pack_%s'%methodName)(value)
+            print value,
+        else:
+            #en estos momentos envíamos el tipo de dato
+            #TODO: debieramos envíar el id del objeto
+            hT.packer.pack_int(dataType)
+            print dataType,
+        print probeId,
+        hT.packer.pack_int(probeId)
+        #print 'parent time stamp = %11.9f'%(parentTimeStamp),
+        print '%11.9f'%(parentTimeStamp),
+        hT.packer.pack_double(parentTimeStamp)        
+        #print 'current depth =',currentDepth,
+        print currentDepth,
+        hT.packer.pack_int(currentDepth)
+        #print 'current time stamp = %11.9f'%(currentTimeStamp),
+        print '%11.9f'%(currentTimeStamp),
+        hT.packer.pack_double(currentTimeStamp)
+        #print 'current thread =',threadId
+        print threadId,
+        hT.packer.pack_int(threadId)
+        object.__setattr__(self, name, value)
+        #se habilita nuevamente settrace
+        sys.settrace(hT.__trace__)
+
+
 if th:
     #a cada nuevo thread se le define settrace
     settrace(hT.__trace__)  
