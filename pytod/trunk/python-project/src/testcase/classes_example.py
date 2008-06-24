654486c5a32d73b5c48c5ef22a6a97aa38242adb
minostro <minostro@52faa79f-a2d0-0310-bd40-81a52920883b>
2008-06-24 22:49:56 +0000

    few changes

--- pytod/trunk/python-project/src/testcase/classes_example.py
+++ pytod/trunk/python-project/src/testcase/classes_example.py
@@ -5,10 +5,11 @@ sys.path.append('/media/WD Passport/eclipse/workspace/python-project/src')
 from debugger.pytod.core.hunterTrace import hT
 
 class clase1(object):
-    
+    h = 2
     def __init__(self, y):
-        self.x = 1
-        self.c = 2
+        self.__x = []
+        self.__x.append(1)
+        self.c = True
         self.z = 1
         self.metodo(self.z, 1, 2, 3)
         return
@@ -30,12 +31,36 @@ class clase1(object):
     
     def foo(self):
         y = 1/0
-    
-"""    
+
+
+class Corriente(object):
+    "Clase Corriente. Nota: Llamar con from pinchpython import *"
+    __num_corrientes = -1
+    __corrientes_definidas = []
+    __tipos_corrientes = ('Caliente','Fr￯﾿ﾽa','Indeterminado')
+    def __init__(self):
+        self.__class__.__num_corrientes += 1
+        self.__MCP = 0
+        self.__T0 = 0
+        self.__Tf = 0
+        self.__h = 0
+        #self.__tipo = self.__tipos_corrientes[2]
+        #self.__class__.__corrientes_definidas.append(self)
+        #self.__id_num = self.__corrientes_definidas.index(self)
+        # N￯﾿ﾽmero de unidades de intercambio de calor necesarias
+        # para esta corriente
+        self.__N_sobre_pinch = 0.0
+        self.__N_bajo_pinch = 0.0
+        self.__phi = 0
+        self.__marcada = False
+        self.__dividida = False
+        self.__vectorAncestros = []
+        self.__vectorAncestros.append(2)
+   
 class clase2(object):
     def __init__(self):
         y = 1
-        
+"""   
         
 class clase3(object):
     def __init__(self):
@@ -43,9 +68,10 @@ class clase3(object):
     def metodo(self):
         v=6
 """
-        
-a = clase1(1)
-a.metodo(10,20,30,40)
-#b = clase2()
-#c = clase3()
-#hT.__printHunter__()
+if __name__ == '__main__':
+    a = clase1(1)
+    a.metodo(10,20,30,40)
+    c = Corriente()
+    #b = clase2()
+    #c = clase3()
+    #hT.__printHunter__()
--- pytod/trunk/python-project/src/testcase/simpleTrace.py
+++ pytod/trunk/python-project/src/testcase/simpleTrace.py
@@ -20,7 +20,7 @@ def __createlnotab__(aCode):
         theLnotab.update({len(aCode.co_code)-1:tuple([last_index,len(aCode.co_code)-1])})                
     return theLnotab 
 
-
+"""
 def trace(frame, event, arg):
     print event
     code = frame.f_code
@@ -53,21 +53,52 @@ def trace(frame, event, arg):
         #print arg
         #raw_input()
         pass
-        
-sys.settrace(trace)
+"""
 
+class Dict(dict):
+    
+    def __setitem__(self, aKey, aValue):
+        print 'estas asignanto',aValue,'a la variable',aKey
+        dict.__setitem__(self,aKey,aValue)
+
+def trace(aFrame, aEvent, aArg):
+    dis.disassemble(aFrame.f_code)
+    if aEvent == 'call':
+        print aFrame.f_code.co_name, aEvent
+        #print aFrame.f_locals
+        #raw_input()
+        if aFrame.f_code.co_name == '__init__':
+            aFrame.f_locals['self'].__dict__ = Dict()
+            type(aFrame.f_locals['self'].__dict__)
+            raw_input()
+        return trace
+    elif aEvent == 'line':
+        print aFrame.f_code.co_name, aEvent
+        #print aFrame.f_locals
+        if aFrame.f_locals.has_key('self'):
+            print aFrame.f_locals['self'].__dict__
+        #raw_input()
+        return trace
+    elif aEvent == 'return':
+        print aFrame.f_code.co_name, aEvent
+        print aFrame.f_locals
+        #print aFrame.f_globals
+        #raw_input()
+    
+#sys.settrace(trace)
+"""
 def m0():
     m1()
 
 def m1():
     m2()
-"""
+
 def m2():
     try:
         m3()
     except:
         print 'hola'
-"""
+        
 def m2():
     m3()
 
@@ -75,3 +106,41 @@ def m3():
     y = 1/0
     
 m0()
+"""
+"""
+class algo(object):
+    a = 0
+
+    def __init__(self):
+        self.b = 52
+        self.z =10
+        print self.a
+    
+    def imprimir(self):
+        print self.b, self.z
+        
+    def __setattr__(self, aName, aValue):
+        print aName, aValue
+        object.__setattr__(self, aName, aValue)
+   
+b = algo()
+algo.imprimir()
+b.e=52
+print b.b
+"""
+
+
+
+class MyDict(dict):
+    def __setitem__(self, aKey, aValue):
+        print 'asignando'
+        dict.__setitem__(self,aKey,aValue)
+        
+    def __update__(self, aDictionary):
+        print 'a'
+
+dicc = MyDict()
+dicc.update({'hola': lambda self: 'hola metamundo!'})
+Saludo = type('Saludo', (), dicc)
+s = Saludo()
+s.hola()
