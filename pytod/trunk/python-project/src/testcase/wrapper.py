75c2caba675d63b0bae03070f390bd6a6ff02823
minostro <minostro@52faa79f-a2d0-0310-bd40-81a52920883b>
2008-06-24 22:50:24 +0000

    added

---
+++ pytod/trunk/python-project/src/testcase/wrapper.py
@@ -0,0 +1,6 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+import sys
+sys.path.append('/media/WD Passport/eclipse/workspace/python-project/src')
+from debugger.pytod.core.hunterTrace import hT
+execfile('classes_example.py')
