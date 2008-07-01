462174428e2f497b7652232aca0c3ede587039b6
gpothier <gpothier@52faa79f-a2d0-0310-bd40-81a52920883b>
2008-07-01 21:58:10 +0000

    Fixed signature calculation

--- agent/trunk/src/native/svn-sig.sh
+++ agent/trunk/src/native/svn-sig.sh
@@ -1,3 +1,5 @@
 #! /bin/sh
 # Compute a signature of the svn revision of the .cpp and .h files
-cat *.cpp *.h | md5sum  | awk '{print $1}'
+FILES="bci-agent.cpp jniutils.cpp jniutils.h md5.cpp md5.h md5_loc.h utils.cpp utils.h"
+
+cat $FILES | md5sum  | awk '{print $1}'
