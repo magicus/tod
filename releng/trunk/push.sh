40e1881084f07b2cd25230b86c649f3adcda85c4
gpothier <gpothier@52faa79f-a2d0-0310-bd40-81a52920883b>
2008-07-02 01:13:47 +0000

    Version 0.5.18

--- releng/trunk/push.sh
+++ releng/trunk/push.sh
@@ -3,4 +3,5 @@
 # This script pushes the release to a server
 
 scp release/* pleiad@pleiad.dcc.uchile.cl:/home/v/pleiad/www/files/tod/releases
-
+VERSION=`ls release/changes_*.txt |awk '{print substr($0, 17, length($0)-20)}'`
+echo ./releng.py -c $VERSION
