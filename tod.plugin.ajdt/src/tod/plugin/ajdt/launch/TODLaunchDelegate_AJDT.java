fe19c95c979a38fa8e6b359716d641eb68e615fb
gpothier <gpothier@52faa79f-a2d0-0310-bd40-81a52920883b>
2008-09-22 23:24:39 +0000

    More JDK1.4 fixes

--- tod.plugin.ajdt/src/tod/plugin/ajdt/launch/TODLaunchDelegate_AJDT.java
+++ tod.plugin.ajdt/src/tod/plugin/ajdt/launch/TODLaunchDelegate_AJDT.java
@@ -36,7 +36,7 @@ public class TODLaunchDelegate_AJDT extends AJApplicationLaunchConfigurationDele
 			IJavaProject theJavaProject = getJavaProject(aConfiguration);
 			if (LaunchUtils.setup(theJavaProject, TODConfigLaunchTab.readConfig(aConfiguration), aLaunch))
 			{
-				super.launch(aConfiguration, LaunchUtils.MODE, aLaunch, aMonitor);
+				super.launch(aConfiguration, LaunchUtils.getLaunchMode(aConfiguration), aLaunch, aMonitor);
 			}
 		}
 		finally
