09086610e9c84f10fa69d5f45396a2c238cff898
gpothier <gpothier@52faa79f-a2d0-0310-bd40-81a52920883b>
2008-10-03 17:40:31 +0000

    Fixed compilation

--- tod.plugin/trunk/src/tod/plugin/launch/ConnectToDatabaseLaunchDelegate.java
+++ tod.plugin/trunk/src/tod/plugin/launch/ConnectToDatabaseLaunchDelegate.java
@@ -18,6 +18,7 @@ import tod.impl.dbgrid.RemoteGridSession;
 import tod.plugin.EclipseProgramLaunch;
 import tod.plugin.TODPlugin;
 import tod.plugin.TODPluginUtils;
+import tod.plugin.views.main.MainView;
 
 public class ConnectToDatabaseLaunchDelegate extends AbstractJavaLaunchConfigurationDelegate
 {
@@ -33,12 +34,13 @@ public class ConnectToDatabaseLaunchDelegate extends AbstractJavaLaunchConfigura
 		
 		theConfig.set(TODConfig.COLLECTOR_HOST, getAddress(aConfiguration));
 		
+		MainView theView = TODPluginUtils.getMainView(true);
+		
 		aMonitor.worked(1);
 		IJavaProject theJavaProject = getJavaProject(aConfiguration);
-		ISession theSession = new RemoteGridSession(null, theConfig, true); 
+		ISession theSession = new RemoteGridSession(theView.getGUIManager(), null, theConfig, true); 
 		theSession.getLaunches().add(new EclipseProgramLaunch(aLaunch, theJavaProject));
 		
-		TODPluginUtils.getMainView(true);
 		TODSessionManager.getInstance().pCurrentSession().set(theSession);
 
 		aMonitor.done();
