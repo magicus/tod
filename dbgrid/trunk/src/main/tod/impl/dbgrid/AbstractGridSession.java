b8f2821bb16b8f9936713f0982006a747742825c
gpothier <gpothier@52faa79f-a2d0-0310-bd40-81a52920883b>
2008-10-06 20:02:47 +0000

    Missing from last commit

---
+++ dbgrid/trunk/src/main/tod/impl/dbgrid/AbstractGridSession.java
@@ -0,0 +1,159 @@
+/*
+TOD - Trace Oriented Debugger.
+Copyright (C) 2006 Guillaume Pothier (gpothier@dcc.uchile.cl)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+version 2 as published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+Parts of this work rely on the MD5 algorithm "derived from the 
+RSA Data Security, Inc. MD5 Message-Digest Algorithm".
+*/
+package tod.impl.dbgrid;
+
+import java.net.URI;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+import javax.swing.JComponent;
+
+import tod.core.config.TODConfig;
+import tod.core.database.browser.ILogBrowser;
+import tod.core.session.AbstractSession;
+import tod.gui.IGUIManager;
+import tod.impl.dbgrid.gui.GridConsole;
+import zz.utils.Utils;
+import zz.utils.monitoring.Monitor.MonitorData;
+import zz.utils.properties.IRWProperty;
+import zz.utils.properties.SimpleRWProperty;
+
+public abstract class AbstractGridSession extends AbstractSession
+{
+	private RIGridMaster itsMaster;
+	private GridLogBrowser itsBrowser;
+
+	private final IRWProperty<Boolean> pCaptureEnabled = 
+		new SimpleRWProperty<Boolean>(this, true)
+		{
+			@Override
+			protected Object canChange(Boolean aOldValue, Boolean aNewValue)
+			{
+				if (getMaster() == null) return aNewValue == null ? ACCEPT : REJECT;
+				else return aNewValue != null ? ACCEPT : REJECT;
+			}
+			
+			@Override
+			protected void changed(Boolean aOldValue, Boolean aNewValue)
+			{
+				try
+				{
+					if (getMaster() == null) return;
+					else getMaster().sendEnableCapture(aNewValue);
+				}
+				catch (RemoteException e)
+				{
+					throw new RuntimeException(e);
+				}
+			}
+		};
+		
+	private MasterListener itsMasterListener; 
+
+	public AbstractGridSession(IGUIManager aGUIManager, URI aUri, TODConfig aConfig)
+	{
+		super(aGUIManager, aUri, aConfig);
+	}
+
+	protected RIGridMaster getMaster()
+	{
+		return itsMaster;
+	}
+	
+	public ILogBrowser getLogBrowser()
+	{
+		return itsBrowser;
+	}
+	
+	protected void setMaster(RIGridMaster aMaster) 
+	{
+		try
+		{
+			if (itsMasterListener == null) itsMasterListener = new MasterListener();
+			
+			if (itsMaster != null) itsMaster.removeListener(itsMasterListener);
+			itsMaster = aMaster;
+			if (itsMaster != null) itsMaster.addListener(itsMasterListener);
+			
+			itsBrowser = itsMaster != null ?
+					DebuggerGridConfig.createRemoteLogBrowser(this, itsMaster)
+					: null;
+		}
+		catch (RemoteException e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+	
+
+	public IRWProperty<Boolean> pCaptureEnabled()
+	{
+		return pCaptureEnabled;
+	}
+
+	public JComponent createConsole()
+	{
+		return new GridConsole(itsMaster);
+	}
+
+	public void flush()
+	{
+		try
+		{
+			itsMaster.flush();
+		}
+		catch (RemoteException e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+
+	private class MasterListener extends UnicastRemoteObject
+	implements RIGridMasterListener
+	{
+		public MasterListener() throws RemoteException
+		{
+		}
+
+		public void eventsReceived() 
+		{
+			itsBrowser.clearStats();
+		}
+
+		public void exception(Throwable aThrowable) 
+		{
+			if (getGUIManager() != null)
+			{
+				getGUIManager().showDialog(new IGUIManager.ErrorDialogType(
+						"Database error",
+						"An error occurred in the database: "+Utils.getRootCause(aThrowable).getMessage()));
+			}
+			
+			aThrowable.printStackTrace();
+		}
+
+		public void monitorData(int aNodeId, MonitorData aData) 
+		{
+		}
+	}
+
+
+}
