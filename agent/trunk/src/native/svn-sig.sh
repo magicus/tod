3f4ef0a20b8a24e455124f9719e16ff98e8e7aa3
gpothier <gpothier@52faa79f-a2d0-0310-bd40-81a52920883b>
2008-11-27 21:09:41 +0000

    Cleaned up native agent source files

--- agent/trunk/src/native/svn-sig.sh
+++ agent/trunk/src/native/svn-sig.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 # Compute a signature of the svn revision of the .cpp and .h files
-FILES="bci-agent.cpp jniutils.cpp jniutils.h md5.cpp md5.h md5_loc.h utils.cpp utils.h"
+FILES="`ls *.cpp *.h`"
 
 cat $FILES | md5sum  | awk '{print $1}'
